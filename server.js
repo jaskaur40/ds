b25ca84eb4bd52e1c46fd115a98f8fa681ec1854
jaskaur40 <jaspreetsaini707@gmail.com>
2017-07-29 21:47:01 -0700

    Updating name

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello. This is jaspreet\n');
+  res.send('Hello. This is Jaspreet. I am running a docker container.\n');
 });
 
 app.listen(PORT, HOST);
